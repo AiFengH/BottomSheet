04b37d6526fa1911cc859ddc4859e08be7da060d
Aaron <h_aifeng@163.com>
2022-06-12 22:44:56 +0800

    Upload botomSheet project.

---
+++ app/src/main/java/com/haf/bottomsheet/MainActivity.kt
@@ -0,0 +1,84 @@
+package com.haf.bottomsheet
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.*
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.haf.bottomsheet.ui.theme.BottomSheetTheme
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+
+    @OptIn(ExperimentalMaterialApi::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BottomSheetTheme {
+                val sheetState = rememberBottomSheetState(initialValue = BottomSheetValue.Collapsed)
+
+                val scaffoldState = rememberBottomSheetScaffoldState(bottomSheetState = sheetState)
+
+                val scope = rememberCoroutineScope()
+
+                BottomSheetScaffold(
+                    scaffoldState = scaffoldState,
+                    sheetContent = {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(300.dp),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(text = "Bottom Sheet!", fontSize = 60.sp)
+                        }
+                    }, sheetBackgroundColor = Color.Blue
+//                    , sheetPeekHeight = 500.dp
+                ) {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Button(onClick = {
+                            scope.launch {
+                                if (sheetState.isCollapsed) {
+                                    sheetState.expand()
+                                } else {
+                                    sheetState.collapse()
+                                }
+                            }
+                        }) {
+                            Text(text = "Bottom sheet fraction ${sheetState.progress.fraction}")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BottomSheetTheme {
+        Greeting("Android")
+    }
+}
